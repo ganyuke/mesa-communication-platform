430a0ca3181dbb01b6cca7a0544d3958dd5ddba4
ganyuke <personalemailforedits@gmail.com>
2022-06-04 18:15:34 -0700

    Mock up the start of a frontend

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,28 @@ from os import path, environ
 db = SQLAlchemy()
 DB_NAME = "database.sqlite3"
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = environ.get("SECRET_KEY", "dev")
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from .views import views
+    app.register_blueprint(views,url_prefix='/')
+
     db.init_app(app)
+    if not path.exists("app/" + DB_NAME):
+        db.create_all(app=app)
+
+    from .models import User
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
     return(app)

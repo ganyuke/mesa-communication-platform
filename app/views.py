359d39fc85ae4c6dfecc962abc823b13204a09e4
ganyuke <personalemailforedits@gmail.com>
2022-06-04 18:32:27 -0700

    Fix broken base page

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from flask import Flask, Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('base.html',user=current_user)
